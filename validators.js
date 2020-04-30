98d0fccba07aa3d94fef91626994fbffefc4ec37
James Florentino <james@warpgate.ventures>
2020-04-30 15:20:01 +0800

    Add validators.isURL

---
+++ validators.js
@@ -0,0 +1,14 @@
+/**
+ * Validates if a string is an HTTP URL
+ * @param {String} str 
+ */
+export const isURL = (str) => {
+  var pattern = new RegExp('^((ft|htt)ps?:\\/\\/)?' + // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name and extension
+    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+    '(\\:\\d+)?' + // port
+    '(\\/[-a-z\\d%@_.~+&:]*)*' + // path
+    '(\\?[;&a-z\\d%@_.,~+&:=-]*)?' + // query string
+    '(\\#[-a-z\\d_]*)?$', 'i') // fragment locator
+  return pattern.test(str)
+}
